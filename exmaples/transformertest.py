6d905420af3b2940b46c1fa68ed4359c04394f0c
NISH1001 <nishanpantha@gmail.com>
2019-05-22 11:19:48 +0545

    Test zoom for fractal tree

--- exmaples/transformertest.py
+++ exmaples/transformertest.py
@@ -10,7 +10,10 @@ sys.path.insert(0, path)
 import numpy as np
 import matplotlib.pyplot as plt
 
-from panim.lsystem import LSystemAnimator
+from panim.lsystem import (
+    LSystemAnimator,
+    BranchedLSystemAnimator
+)
 from panim.transformers import ZoomTransformer
 
 plt.style.use('dark_background')
@@ -47,22 +50,38 @@ def test():
     iteration = 3
     n = 75
 
-    animator = LSystemAnimator(
+    angle = 22.5
+    axiom = 'F'
+    rule = {
+        'F': ' FF+[+F-F-F]-[-F+F+F]'
+    }
+    iteration = 5
+
+    # animator = LSystemAnimator(
+    #     interval=5,
+    #     iteration=iteration,
+    #     rule=rule,
+    #     axiom=axiom,
+    #     turn_angle=angle,
+    #     # start_position=(0, -10),
+    #     # nlimit=n,
+    # )
+    animator = BranchedLSystemAnimator(
         interval=5,
         iteration=iteration,
         rule=rule,
         axiom=axiom,
         turn_angle=angle,
-        # start_position=(0, -10),
-        # nlimit=n,
+        start_position=(0, -20),
+        nlimit=40
     )
 
     zoomer = ZoomTransformer(
         animobj=animator,
-        factor=500,
+        factor=2000,
         nlimit=n)
     zoomer.animate(len(animator.coords))
-    zoomer.save("out/zoom-test.mp4")
+    zoomer.save("out/zoom-test-tree.mp4")
 
 
 def main():
