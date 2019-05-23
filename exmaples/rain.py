c9426a1af4273c51e8bddcc8b8963aa7df192ab9
NISH1001 <nishanpantha@gmail.com>
2019-05-23 14:03:52 +0545

    Add simple rain animation
    Add `RainAnimator` to simulate simple circular rain drops. For now the
    code is messy since the base class `AbstractAnimator` currently has only
    support for line plot instead of scatter plot. Have to modify the base
    to support both types.

---
+++ exmaples/rain.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys
+from pathlib import Path
+
+path = str(Path().absolute())
+print("Adding {} to system path...".format(path))
+sys.path.insert(0, path)
+
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('dark_background')
+
+from panim.rain import RainAnimator
+
+
+def main():
+    r = RainAnimator()
+    r.animate(500)
+    r.save("out/rain.mp4")
+
+if __name__ == "__main__":
+    main()
+
---
+++ panim/rain.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
+from panim.animator import AbstractAnimator
+
+plt.style.use('dark_background')
+
+class RainAnimator:
+    def __init__(self, **args):
+        super().__init__(**args)
+        ndrops = args.get('ndrops', 50)
+        self.interval = args.get('interval', 10)
+        # Create new Figure and an Axes which fills it.
+        self.fig = plt.figure(figsize=(7, 7))
+        self.ax = self.fig.add_axes([0, 0, 1, 1], frameon=False)
+        self.ax.set_xlim(0, 1), self.ax.set_xticks([])
+        self.ax.set_ylim(0, 1), self.ax.set_yticks([])
+        self.ndrops = ndrops
+        self.drops = np.zeros(ndrops, dtype=[('position', float, 2),
+                                      ('size',     float, 1),
+                                      ('growth',   float, 1),
+                                      ('color',    float, 4)])
+        self.drops['position'] = np.random.uniform(0, 1, (ndrops, 2))
+        self.drops['growth'] = np.random.uniform(50, 200, ndrops)
+        self.img = self.ax.scatter(self.drops['position'][:, 0], self.drops['position'][:, 1],
+                        s=self.drops['size'], lw=0.5, edgecolors=self.drops['color'],
+                        facecolors='none')
+
+    def update(self, i):
+        # Get an index which we can use to re-spawn the oldest raindrop.
+        print(i)
+        current_index = i % self.ndrops
+
+        # Make all colors more transparent as time progresses.
+        self.drops['color'][:, 3] -= 1.0/len(self.drops)
+        self.drops['color'][:, 3] = np.clip(self.drops['color'][:, 3], 0, 1)
+
+        # Make all circles bigger.
+        self.drops['size'] += self.drops['growth']
+
+        # Pick a new position for oldest rain drop, resetting its size,
+        # color and growth factor.
+        self.drops['position'][current_index] = np.random.uniform(0, 1, 2)
+        self.drops['size'][current_index] = 5
+        self.drops['color'][current_index] = (0, 1, 1, 1)
+        self.drops['growth'][current_index] = np.random.uniform(50, 200)
+
+        # Update the scatter collection, with the new colors, sizes and positions.
+        self.img.set_edgecolors(self.drops['color'])
+        self.img.set_sizes(self.drops['size'])
+        self.img.set_offsets(self.drops['position'])
+
+    def animate(self, num_frames=1000):
+        self.num_frames = num_frames
+        plt.axis('off')
+        self.anim = animation.FuncAnimation(self.fig, self.update,
+                               frames=num_frames, interval=self.interval, blit=False,
+                               repeat=False)
+
+    def save(self, filename="out/animation.mp4", fps=30, dpi=100):
+        writer = animation.writers['ffmpeg'](fps=fps)
+        # self.anim.save(filename, writer='imagemagick')
+        self.anim.save(filename, writer=writer, dpi=dpi)
+        print("Saving {} to {}".format(self.__class__.__name__, filename))
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
